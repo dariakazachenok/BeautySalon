f9e7f19d38bf99f5cc423a89f719199860779625
daria kazachenok <kazachenokd@mail.ru>
2018-02-26 14:12:10 +0300

    add new project

---
+++ BeautySalon/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BeautySalon.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
