a6ae0760469d5a8cfefe5d71f59f6f988568342f
daria kazachenok <kazachenokd@mail.ru>
2018-05-16 16:29:51 +0300

    fix view services

--- BeautySalon/Startup.cs
+++ BeautySalon/Startup.cs
@@ -3,7 +3,6 @@ using Identity;
 using Identity.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
